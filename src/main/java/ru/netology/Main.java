6fd54cb0eedc4c5e3380e1a25c3a137eae662218
KosNet75 <nemokonstantin@gmail.com>
2022-12-28 17:54:25 +0700

    Final commit

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,9 +3,9 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) {
         Ints intsCalc = new IntsCalculator();
-        System.out.println(intsCalc.sum(2, 8));
-        System.out.println(intsCalc.mult(10, 33));
-        System.out.println(intsCalc.pow(2, 10));
+        System.out.println(intsCalc.sum(2, 18));
+        System.out.println(intsCalc.mult(10, 12));
+        System.out.println(intsCalc.pow(3, 3));
 
     }
 }
